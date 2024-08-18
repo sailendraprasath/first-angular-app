6038ff37caf874db505126b94a5189d7b7b4e193
sailendraprasath <sailendraprasath05@gmail.com>
2024-08-18 11:00:06 +0530

    hlo

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
+import { TaskModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, TaskModule],
+})
+export class AppModule {}
